0e32d9e1d1cc5139fb66885b6415e18c301e2997
Ratnesh Chouhan <Ratnesh.gfg@gmail.com>
2023-03-02 21:21:56 +0000

    Initial Commit

---
+++ crud-with-square-test/src/main/java/com/rc/tech/crudwithsquaretest/CrudWithSquareTestApplication.java
@@ -0,0 +1,13 @@
+package com.rc.tech.crudwithsquaretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithSquareTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithSquareTestApplication.class, args);
+	}
+
+}
---
+++ crud-with-square-test/src/test/java/com/rc/tech/crudwithsquaretest/CrudWithSquareTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.tech.crudwithsquaretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithSquareTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
